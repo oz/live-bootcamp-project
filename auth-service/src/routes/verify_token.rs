2767e6c5ce56efeb4900a2a4899e89ff02d61f3d
Arnaud Berthomier <oz@cyprio.net>
2025-10-21 19:05:05 -0600

    feat: verify token endpoint

--- auth-service/src/routes/verify_token.rs
+++ auth-service/src/routes/verify_token.rs
@@ -1,5 +1,16 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::{Json, http::StatusCode, response::IntoResponse};
+use serde::Deserialize;
 
-pub async fn verify_token() -> impl IntoResponse {
-    StatusCode::OK.into_response()
+use crate::{domain::AuthAPIError, utils};
+
+pub async fn verify_token(Json(request): Json<VerifyTokenRequest>) -> impl IntoResponse {
+    match utils::auth::validate_token(&request.token).await {
+        Ok(_) => Ok(StatusCode::OK),
+        Err(_) => Err(AuthAPIError::InvalidToken),
+    }
+}
+
+#[derive(Deserialize)]
+pub struct VerifyTokenRequest {
+    pub token: String,
 }
--- auth-service/tests/api/verify_token.rs
+++ auth-service/tests/api/verify_token.rs
@@ -1,10 +1,53 @@
-use crate::helpers::TestApp;
+use auth_service::utils::constants::JWT_COOKIE_NAME;
+
+use crate::helpers::{TestApp, get_random_email};
 
 #[tokio::test]
-async fn verify_token_returns_200() {
+async fn should_return_422_if_malformed_input() {
     let app = TestApp::new().await;
+    let body = serde_json::json!({ "invalid": "input", });
 
-    let response = app.post_verify_token().await;
+    let response = app.post_verify_token(&body).await;
+    assert_eq!(response.status().as_u16(), 422);
+}
 
+#[tokio::test]
+async fn should_return_200_valid_token() {
+    // Create a user
+    let random_email = get_random_email();
+    let signup_body = serde_json::json!({
+        "email": random_email,
+        "password": "password123",
+        "requires2FA": false
+    });
+    let app = TestApp::new().await;
+    let response = app.post_signup(&signup_body).await;
+    assert_eq!(response.status().as_u16(), 201);
+
+    // Login to get a token
+    let login_body = serde_json::json!({
+        "email": random_email,
+        "password": "password123",
+    });
+    let response = app.post_login(&login_body).await;
     assert_eq!(response.status().as_u16(), 200);
+
+    // Verify the token we just received...
+    let auth_cookie = response
+        .cookies()
+        .find(|cookie| cookie.name() == JWT_COOKIE_NAME)
+        .expect("No auth cookie found");
+    assert!(!auth_cookie.value().is_empty());
+
+    let body = serde_json::json!({ "token": auth_cookie.value(), });
+    let response = app.post_verify_token(&body).await;
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn should_return_401_if_invalid_token() {
+    let app = TestApp::new().await;
+    let body = serde_json::json!({ "token": "does not exist", });
+    let response = app.post_verify_token(&body).await;
+    assert_eq!(response.status().as_u16(), 401);
 }
