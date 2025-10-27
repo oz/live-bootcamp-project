099babe537dc403bf3775fd55783b70686154164
Arnaud Berthomier <oz@cyprio.net>
2025-10-27 17:48:00 -0600

    feat: return 206 on login when 2fa is required

--- auth-service/src/routes/login.rs
+++ auth-service/src/routes/login.rs
@@ -1,11 +1,11 @@
-use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use axum::{Json, extract::State, http::StatusCode, response::IntoResponse};
 use axum_extra::extract::CookieJar;
 use serde::{Deserialize, Serialize};
 
 use crate::utils;
 use crate::{
-    domain::{email::Email, password::Password, AuthAPIError},
     AppState,
+    domain::{AuthAPIError, email::Email, password::Password},
 };
 
 pub async fn login(
@@ -21,18 +21,59 @@ pub async fn login(
     };
 
     let user_store = &state.user_store.read().await;
-    match user_store.get_user(email).await {
-        Ok(user) if user.password == password => {
-            let auth_cookie = utils::auth::generate_auth_cookie(&(user.email));
-            if auth_cookie.is_err() {
-                return (jar, Err(AuthAPIError::UnexpectedError));
-            }
-            let auth_cookie = auth_cookie.unwrap();
-            let updated_jar = jar.add(auth_cookie);
-            (updated_jar, Ok(StatusCode::OK))
-        }
-        _ => (jar, Err(AuthAPIError::IncorrectCredentials)),
+    let user = match user_store.get_user(email).await {
+        Ok(user) if user.password == password => user,
+        _ => return (jar, Err(AuthAPIError::IncorrectCredentials)),
+    };
+
+    // Handle request based on user's 2FA configuration
+    match user.requires_2fa {
+        true => handle_2fa(jar).await,
+        false => handle_no_2fa(&user.email, jar).await,
     }
+}
+
+// New!
+async fn handle_2fa(
+    jar: CookieJar,
+) -> (
+    CookieJar,
+    Result<(StatusCode, Json<LoginResponse>), AuthAPIError>,
+) {
+    // TODO: Return a TwoFactorAuthResponse. The message should be "2FA required".
+    // The login attempt ID should be "123456". We will replace this hard-coded login attempt ID soon!
+    let response = TwoFactorAuthResponse {
+        message: "2FA required".to_owned(),
+        login_attempt_id: "123456".to_owned(),
+    };
+
+    (
+        jar,
+        Ok((
+            StatusCode::PARTIAL_CONTENT,
+            Json(LoginResponse::TwoFactorAuth(response)),
+        )),
+    )
+}
+
+// New!
+async fn handle_no_2fa(
+    email: &Email,
+    jar: CookieJar,
+) -> (
+    CookieJar,
+    Result<(StatusCode, Json<LoginResponse>), AuthAPIError>,
+) {
+    let auth_cookie = utils::auth::generate_auth_cookie(email);
+    if auth_cookie.is_err() {
+        return (jar, Err(AuthAPIError::UnexpectedError));
+    }
+    let auth_cookie = auth_cookie.unwrap();
+    let updated_jar = jar.add(auth_cookie);
+    (
+        updated_jar,
+        Ok((StatusCode::OK, Json(LoginResponse::RegularAuth))),
+    )
 }
 
 #[derive(Deserialize)]
@@ -41,7 +82,17 @@ pub struct LoginRequest {
     pub password: String,
 }
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
-pub struct LoginResponse {
+#[derive(Debug, Serialize)]
+#[serde(untagged)]
+pub enum LoginResponse {
+    RegularAuth,
+    TwoFactorAuth(TwoFactorAuthResponse),
+}
+
+// If a user requires 2FA, this JSON body should be returned!
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TwoFactorAuthResponse {
     pub message: String,
+    #[serde(rename = "loginAttemptId")]
+    pub login_attempt_id: String,
 }
--- auth-service/tests/api/login.rs
+++ auth-service/tests/api/login.rs
@@ -1,5 +1,5 @@
 use crate::helpers::{TestApp, get_random_email};
-use auth_service::utils::constants::JWT_COOKIE_NAME;
+use auth_service::{routes::TwoFactorAuthResponse, utils::constants::JWT_COOKIE_NAME};
 
 #[tokio::test]
 async fn should_return_422_if_malformed_credentials() {
@@ -66,4 +66,36 @@ async fn should_return_200_if_valid_credentials_and_2fa_disabled() {
         .expect("No auth cookie found");
 
     assert!(!auth_cookie.value().is_empty());
+}
+
+#[tokio::test]
+async fn should_return_206_if_valid_credentials_and_2fa_enabled() {
+    let app = TestApp::new().await;
+
+    // Create an account with 2FA enabled.
+    let random_email = get_random_email();
+    let signup_body = serde_json::json!({
+        "email": random_email,
+        "password": "password123",
+        "requires2FA": true
+    });
+    let response = app.post_signup(&signup_body).await;
+    assert_eq!(response.status().as_u16(), 201);
+
+    let login_body = serde_json::json!({
+        "email": random_email,
+        "password": "password123",
+    });
+
+    // Login should return a 206
+    let response = app.post_login(&login_body).await;
+    assert_eq!(response.status().as_u16(), 206);
+    assert_eq!(
+        response
+            .json::<TwoFactorAuthResponse>()
+            .await
+            .expect("Could not deserialize response body to TwoFactorAuthResponse")
+            .message,
+        "2FA required".to_owned()
+    );
 }
